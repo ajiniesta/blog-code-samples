f1e1913077d654b08cadb9a60a63d45585e268b3
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2015-10-27 21:43:17 +0100

    Removing hadoop-common maven dependency and adding logs

--- quickmr/src/main/java/com/iniesta/blogsamples/quickmr/QuickDriver.java
+++ quickmr/src/main/java/com/iniesta/blogsamples/quickmr/QuickDriver.java
@@ -10,10 +10,26 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 public class QuickDriver extends Configured implements Tool{
 
+	private static void initLogs() {
+		ConsoleAppender console = new ConsoleAppender(); // create appender
+		// configure the appender
+		String PATTERN = "%d [%p|%c|%C{1}] %m%n";
+		console.setLayout(new PatternLayout(PATTERN));
+		console.setThreshold(Level.INFO);
+		console.activateOptions();
+		// add appender to any Logger (here is root)
+		Logger.getRootLogger().addAppender(console);
+	}
+	
 	public static void main(String[] args) throws Exception {
+		initLogs();
 		int exitCode = ToolRunner.run(new Configuration(), new QuickDriver(), args);
 		System.exit(exitCode);
 	}
